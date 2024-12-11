c35b669002e40ad2a8211e1def4c1a935ef91186
vita <the777the7@gmail.com>
2024-12-11 14:38:49 +0900

    main

--- src/Main.js
+++ src/Main.js
@@ -1,69 +1,90 @@
-import { useState, EventHandler, ReactNode } from 'react'
+import { useState, useEffect, EventHandler, ReactNode } from 'react'
 import './output.css';
 
 const Main = () => {
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
+  const words = ['노원으로부터', '공릉으로부터', '경춘선숲길로부터', '월계로부터','프나온으로부터']; // 교체할 단어 리스트
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTextIndex((prevIndex) => (prevIndex + 1) % words.length);
+    }, 2000); // 2초 애니메이션 + 2초 대기
+    return () => clearInterval(interval);
+  }, []);
+
 	return (<div className="w-screen h-[3774px] bg-[#fff] overflow-hidden">
-  <div className="absolute left-[642px] top-[1887px] text-[40px] font-['Pretendard'] font-bold text-[#fff] whitespace-nowrap">CURTURE IN NOWON</div>
-  <div className="absolute left-[-2px] top-[157px] w-[1444px] flex flex-col items-center justify-start gap-[36px]">
+  <div className="absolute top-[157px] w-screen flex flex-col items-center justify-start gap-[36px]">
     <div className="relative w-screen h-[769px] shrink-0 flex">
-      <div className="absolute left-[323px] top-0 w-[460px] h-[682px] flex">
-        <div className="absolute left-[33.44%] right-0 top-[60.5%] bottom-[3.23%] bg-[#c5ff31]"></div>
-        <div className="absolute left-[23.66%] right-[24.07%] top-[39.63%] bottom-[20.86%] bg-[#c5ff31]"></div>
-        <img className="absolute left-0 right-[57.77%] top-[62.86%] bottom-[0%]" width="194" height="253" src="월계53_10.png"></img>
-        <div className="absolute left-[8.15%] right-[20.79%] top-0 bottom-[26.62%] bg-[#c5ff31]"></div>
+      <div className="absolute left-1/2 top-0 items-center w-[460px] h-[682px] flex">
+        
+        <img className="relative -translate-x-1/2 top-[40px]" width="600"  src={process.env.PUBLIC_URL + "/bg_map.png"}></img>
+
       </div>
       <div className="absolute -translate-x-1/2 left-1/2 top-[40px] flex flex-col items-center justify-center">
-        <div className="w-[1114px] text-[150px] font-['Pretendard'] font-bold text-[#000] text-center">FROM NOW ON</div>
-        <div className="w-[1114px] h-[188px] text-[150px] font-['Pretendard'] font-bold text-[#000] text-center">지금,</div>
-        <div className="w-[1114px] h-[188px] text-[150px] font-['Pretendard'] font-bold text-[#000] text-center">노원으로부터</div>
+        <div className="w-[1114px] h-[180px] text-[150px] font-['Pretendard'] font-bold text-[#000] text-center">FROM NOW ON</div>
+        <div className="w-[1114px] h-[180px] text-[150px] font-['Pretendard'] font-bold text-[#000] text-center">지금,</div>
+        <div className="w-[1114px] h-[180px] text-[150px] font-['Pretendard'] font-bold text-[#000] text-center overflow-hidden relative">
+              <span className="relative h-full flex flex-col items-center">
+                {words.map((word, index) => (
+                  <span 
+                    key={index} 
+                    className={`absolute transition-transform duration-1000 ease-in-out ${index === currentTextIndex ? 'translate-y-0' : 'translate-y-[100%]'}`}
+                  >
+                    {word}
+                  </span>
+                ))}
+              </span> </div>
         <div className="text-[36px] font-['Pretendard'] text-[#000] whitespace-nowrap">노원구 중심 감도 높은 노원구 공간 큐레이션 매거진, 프롬나우온</div>
         <div className="flex flex-row items-start justify-start gap-[36px] py-[30px] px-0">
           <div className="w-[250px] h-[70px] shrink-0 flex flex-row items-center justify-between py-[10px] px-[20px] bg-[#fff] border-[1px] border-solid border-[#000]">
             <div className="text-[25px] font-['Pretendard'] text-[#000] text-center whitespace-nowrap">구독하러 가기</div>
-            <img width="25" height="22" src="Vector89_327.png"></img>
+            <img width="25" height="22" src={process.env.PUBLIC_URL + "/right.png"}></img>
           </div>
           <div className="w-[250px] h-[70px] shrink-0 flex flex-row items-center justify-between py-[10px] px-[20px] bg-[#fff] border-[1px] border-solid border-[#000]">
             <div className="text-[25px] font-['Pretendard'] text-[#000] text-center whitespace-nowrap">인스타그램</div>
-            <img width="25" height="22" src="Vector89_332.png"></img>
+            <img width="25" height="22" src={process.env.PUBLIC_URL + "/right.png"}></img>
           </div>
         </div>
       </div>
     </div>
-    <div className="self-stretch flex flex-row items-center justify-between">
-      <div className="relative w-[840px] h-[980px] shrink-0 flex flex-col items-start justify-between py-[30px] px-[31px]">
-        <div className="absolute left-0 top-0 w-[840px] h-[980px] shrink-0 flex">
-          <img className="absolute left-0 top-0" width="840" height="980" src="image 737_8.png"></img>
-          <div className="w-[840px] h-[980px] bg-[linear-gradient(180deg,#00000000_0%,#00000044_60%,#000_100%)]"></div>
+
+    <div className="w-screen flex flex-row items-start justify-between">
+        <div className="relative w-[60%] h-auto shrink-0 flex flex-col items-end justify-between p-4">
+          <div className="relative w-full h-auto flex">
+            <img className="w-full h-auto" src={process.env.PUBLIC_URL + "/main_post1.png"} alt="main post" />
+            <div className="w-full h-auto bg-gradient-to-b from-transparent via-black/60 to-black"></div>
+          </div>
+          <div className="flex flex-row items-center justify-start gap-4">
+            <div className="text-2xl font-['Pretendard'] font-bold text-white whitespace-nowrap">F&B PLACE</div>
+            <img width="31" height="28" src="Vector64_29.png" alt="icon" />
+          </div>
+          <div className="w-full flex flex-col items-start justify-start gap-2">
+            <div className="w-full text-4xl font-['Pretendard'] font-extrabold text-white">이탈리아 현지 생면의 맛을 찾아서</div>
+            <div className="w-full text-xl font-['Inter'] text-white">공릉 페페그라노</div>
+          </div>
         </div>
-        <div className="flex flex-row items-center justify-start gap-[17px]">
-          <div className="text-[40px] font-['Pretendard'] font-bold text-[#fff] whitespace-nowrap">F&B PLACE</div>
-          <img width="31" height="28" src="Vector64_29.png"></img>
-        </div>
-        <div className="w-[655px] flex flex-col items-start justify-start gap-[5px]">
-          <div className="self-stretch h-[64px] text-[50px] font-['Pretendard'] font-extrabold text-[#fff] flex flex-col justify-end">이탈리아 현지 생면의 맛을 찾아서</div>
-          <div className="self-stretch h-[32px] text-[20px] font-['Inter'] text-[#fff]">공릉 페페그라노</div>
+        <div className="relative w-[40%] h-auto shrink-0 flex">
+          <div className="absolute w-full h-full bg-white"></div>
+          <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-32 h-14 text-4xl font-['Inter'] text-black">지도?</div>
+          <div className="absolute w-full h-full overflow-hidden">
+            <div className="absolute inset-0 bg-gray-400"></div>
+            <div className="absolute inset-y-0 left-1/4 right-1/4 bg-gray-300"></div>
+            <div className="absolute inset-x-0 bottom-0 bg-gray-300"></div>
+            <div className="absolute top-0 bottom-1/4 left-1/12 right-1/5 bg-gray-300"></div>
+            <div className="absolute top-1/5 bottom-2/3 left-1/3 right-1/3 text-xl font-['Pretendard'] font-semibold text-white">상계</div>
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 text-xl font-['Pretendard'] font-semibold text-white">중계</div>
+            <div className="absolute bottom-1/4 left-1/3 right-1/3 text-xl font-['Pretendard'] font-semibold text-white">하계</div>
+            <div className="absolute bottom-1/6 left-1/6 text-xl font-['Pretendard'] font-semibold text-white">월계</div>
+            <div className="absolute bottom-1/6 right-1/6 text-4xl font-['Pretendard'] font-semibold text-black">공릉</div>
+          </div>
+          <div className="absolute top-4 left-4 flex flex-row items-center justify-start gap-4">
+            <div className="text-2xl font-['Pretendard'] font-bold text-black">MAP</div>
+            <img width="31" height="28" src="Vector64_36.png" alt="icon" />
+          </div>
         </div>
       </div>
-      <div className="relative w-[600px] h-[980px] shrink-0 flex">
-        <div className="absolute left-0 top-0 w-[600px] h-[980px] bg-[#fff]"></div>
-        <div className="absolute left-[237px] top-[437px] w-[126px] h-[53px] text-[40px] font-['Inter'] text-[#000]">지도?</div>
-        <div className="absolute left-[57px] top-[142px] w-[486px] h-[706px] overflow-hidden">
-          <div className="absolute left-[33.44%] right-[-0%] top-[60.5%] bottom-[3.23%] bg-[#c5ff31]"></div>
-          <div className="absolute left-[23.66%] right-[24.07%] top-[39.63%] bottom-[20.86%] bg-[#d9d9d9]"></div>
-          <div className="absolute left-0 right-[57.77%] top-[62.86%] bottom-[-0%] bg-[#d9d9d9]"></div>
-          <div className="absolute left-[8.15%] right-[20.79%] top-0 bottom-[26.62%] bg-[#d9d9d9]"></div>
-          <div className="absolute left-[35.64%] right-[35.78%] top-[22.85%] bottom-[65.62%] text-[20px] font-['Pretendard'] font-semibold text-[#fff]">상계</div>
-          <div className="absolute left-[53.61%] right-[17.81%] top-[51.27%] bottom-[37.21%] text-[20px] font-['Pretendard'] font-semibold text-[#fff]">중계</div>
-          <div className="absolute left-[35.35%] right-[36.07%] top-[65.37%] bottom-[23.1%] text-[20px] font-['Pretendard'] font-semibold text-[#fff]">하계</div>
-          <div className="absolute left-[16.2%] right-[55.22%] top-[78.69%] bottom-[9.79%] text-[20px] font-['Pretendard'] font-semibold text-[#fff]">월계</div>
-          <div className="absolute left-[61.72%] right-[9.71%] top-[74.89%] bottom-[13.58%] text-[48px] font-['Pretendard'] font-semibold text-[#000]">공릉</div>
-        </div>
-        <div className="absolute left-[30px] top-[22px] flex flex-row items-center justify-start gap-[14px]">
-          <div className="text-[40px] font-['Pretendard'] font-bold text-[#000] whitespace-nowrap">MAP</div>
-          <img width="31" height="28" src="Vector64_36.png"></img>
-        </div>
-      </div>
-    </div>
+
+
     <div className="self-stretch flex flex-row items-center justify-start gap-[1px]">
       <div className="w-[600px] h-[980px] shrink-0 flex flex-col items-start justify-between pt-[31px] pr-[20px] pb-0 pl-[31px] bg-[#fff]">
         <div className="w-[379px] flex flex-col items-start justify-start gap-[10px]">
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}", // React의 모든 파일 포함
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        flip: {
+          '0%': { transform: 'rotateX(0deg)' },
+          '50%': { transform: 'rotateX(90deg)', opacity: 0 },
+          '100%': { transform: 'rotateX(0deg)', opacity: 1 },
+        }
+      },
+      animation: {
+        flip: 'flip 2s ease-in-out infinite',
+      }
+    }
   },
   plugins: [],
+  
 };
